e71f1972e2511a743f345f4f092624c43d91b26f
diegorosah <diegorosah.hc@gmail.com>
2020-10-23 21:45:41 -0300

    Inlcusao Framework

--- FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
+++ FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
@@ -1,14 +1,7 @@
 package br.com.framework.pages;
 
 public class Metodos {
-String nome;
 
-public String getNome() {
-	return nome;
-}
-
-public void setNome(String nome) {
-	this.nome = nome;
 }
 }
 
