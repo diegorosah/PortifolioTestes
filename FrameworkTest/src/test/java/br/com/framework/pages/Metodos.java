cc2ad97ca3f5e26a7d558f3d2e8253f58895e981
diegorosah <diegorosah.hc@gmail.com>
2020-10-23 21:37:52 -0300

    Teste

---
+++ FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
@@ -0,0 +1,14 @@
+package br.com.framework.pages;
+
+public class Metodos {
+String nome;
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+}
+
