50d3af0a3eab2eecbc12cc5612a5350bf82ec779
diegorosah <diegorosah.hc@gmail.com>
2020-10-28 21:30:27 -0300

    Retestes

--- FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
+++ FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
@@ -3,13 +3,12 @@ package br.com.framework.pages;
 import org.openqa.selenium.WebDriver;
 
 public class Metodos {
-	
+
 	WebDriver driver;
-	
+
 	public void navegador(String url, String navegador) {
+		blablabla
 		
 	}
 
 }
-
-
